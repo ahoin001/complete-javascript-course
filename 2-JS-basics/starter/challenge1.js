d1c694512dfecdc8d4200137ea8bc9c5bd14a693
Alexander Hoinville <ahoin001@gmail.com>
2019-08-25 21:47:29 -0400

    Coding challenge 3

--- 2-JS-basics/starter/challenge1.js
+++ 2-JS-basics/starter/challenge1.js
@@ -1,17 +1,18 @@
 // console.log(`Hello World From External Sheet`);
+/*****************************
+* CODING CHALLENGE 1
+*/
 
-// ********************************************************************** 
-// CODING CHALLENGE 1 
+/*
+Mark and John are trying to compare their BMI (Body Mass Index), which is calculated using the formula: BMI = mass / height^2 = mass / (height * height). (mass in kg and height in meter).
 
-// Mark and John want to compare BMI, 
-// BMI = mass / height^2 ( mass / hiehgt * height)
+1. Store Mark's and John's mass and height in variables
+2. Calculate both their BMIs
+3. Create a boolean variable containing information about whether Mark has a higher BMI than John.
+4. Print a string to the console containing the variable from step 3. (Something like "Is Mark's BMI higher than John's? true"). 
 
-// 1. Save both bmi's to variable
-// 2. Calaculate both their BMI's
-// 3. Boolean deciding whether mak has higher bmi than john 
-// 4. Print a string to the console containing the booolean from step 3
-
-// **********************************************************************
+GOOD LUCK 😀
+*/
 
 let markMass = 70;
 let markHeight = 1.2;
@@ -25,12 +26,10 @@ let johnBMI = johnMass / (johnHeight * johnHeight);
 // Boolean for determining Mark is heavier
 let markBMIIsHeavier;
 
-if (markBMI > johnBMI)
-{
+if (markBMI > johnBMI) {
     markBMIIsHeavier = true;
 }
-else
-{
+else {
     markBMIIsHeavier = false;
 }
 
