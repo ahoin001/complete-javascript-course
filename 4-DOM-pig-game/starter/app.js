026c623d262a690c128bf55b28749e5f9654f821
Alexander Hoinville <ahoin001@gmail.com>
2019-08-30 20:21:31 -0400

    Add logic for roll dice

--- 4-DOM-pig-game/starter/app.js
+++ 4-DOM-pig-game/starter/app.js
@@ -9,6 +9,13 @@ GAME RULES:
 
 */
 
+// Clear all scores
+document.getElementById('current-0').innerHTML = 0;
+document.getElementById('score-0').innerHTML = 0;
+document.getElementById('current-1').innerHTML = 0;
+document.getElementById('score-1').innerHTML = 0;
+
+// Global variables for use
 let globalScores,
     roundScore,
     activePlayer;
@@ -17,12 +24,6 @@ let globalScores,
 globalScores = [0, 0];
 roundScore = 0;
 activePlayer = 0;
-
-// Clear all scores
-document.getElementById('current-0').innerHTML = 0;
-document.getElementById('score-0').innerHTML = 0;
-document.getElementById('current-1').innerHTML = 0;
-document.getElementById('score-1').innerHTML = 0;
 
 // Hide dice until we roll
 let theDice = document.querySelector('.dice');
@@ -35,14 +36,28 @@ rollButton.addEventListener('click', () => {
     // 1. random number from 1-6 inclusive
     dice = Math.floor(Math.random() * 6) + 1;
 
-    // 2. Display result in current score of active player
-    document.querySelector('#current-' + activePlayer).innerHTML = dice;
-
-    // 3. display proper dice image reflecting //#endregion
+    // 2. Display Dice result
     theDice.style.display = 'block';
-    theDice.src = 'dice-' + dice +'.png';
+    theDice.src = 'dice-' + dice + '.png';
 
-    // 4.
+    // 3.  
+    if (dice !== 1) {
 
+        // Add dice number to round score of that player whenever dice is'nt equal too 1
+        roundScore += dice;
+        document.querySelector('#current-' + activePlayer).innerHTML = roundScore;
+
+    }
+    else {
+
+        // Change active player
+        if (activePlayer === 0) {
+            activePlayer = 1;
+        }
+        else if (activePlayer === 1) {
+            activePlayer = 0;
+        }
+
+    }
 
 })
