8642bfb3dad04382c60216e9bc0c8eb40099349b
Alexander Hoinville <ahoin001@gmail.com>
2019-09-02 20:14:30 -0400

    Added closure Practice

--- 5-advanced-JS/starter/script.js
+++ 5-advanced-JS/starter/script.js
@@ -101,47 +101,187 @@ if (ex1) {
 // *****************************************************************
 if (ex1) {
 
-    // Lecture: Passing functions as arguments
+    // // Lecture: Passing functions as arguments
 
-    var years = [1990, 1995, 2000, 2005, 2010];
+    // var years = [1990, 1995, 2000, 2005, 2010];
 
-    // Pass an array and a callback function into this function
-    function arrayCalc(arr, callBack) {
+    // // Pass an array and a callback function into this function
+    // function arrayCalc(arr, callBack) {
 
-        // Empty array created
-        var arrRes = [];
+    //     // Empty array created
+    //     var arrRes = [];
 
-        // Loop through the array that was passed in 
-        // And manipulate each element with whatever callback function is passed in 
-        for (var i = 0; i < arr.length; i++) {
-            arrRes.push(callBack(arr[i]));
+    //     // Loop through the array that was passed in 
+    //     // And manipulate each element with whatever callback function is passed in 
+    //     for (var i = 0; i < arr.length; i++) {
+    //         arrRes.push(callBack(arr[i]));
+    //     }
+
+    //     return arrRes;
+    // }
+
+    // function calculateAge(argument) {
+    //     return 2019 - argument;
+    // }
+
+    // function isAdult(argument) {
+    //     return argument >= 18;
+    // }
+
+    // function maxHeartRate(argument) {
+    //     if (argument >= 18 && argument <= 81) {
+    //         return Math.round(206.9 - (0.67 * argument));
+    //     } else {
+    //         return -1;
+    //     }
+    // }
+
+    // // Call function with same array but different functions
+    // var ages = arrayCalc(years, calculateAge);
+    // var fullAges = arrayCalc(ages, isAdult);
+    // var rates = arrayCalc(ages, maxHeartRate);
+
+    // console.log(`Ages: ${ages}`);
+    // console.log(`Rates: ${rates}`);
+
+}
+
+// *****************************************************************
+//    Lecture: Functions returning functions
+// *****************************************************************
+if (ex1) {
+
+    // function interviewQuestion(job) {
+    //     if (job === 'designer') {
+    //         return function (name) {
+    //             console.log(name + ', can you please explain what UX design is?');
+    //         }
+    //     } else if (job === 'teacher') {
+    //         return function (name) {
+    //             console.log('What subject do you teach, ' + name + '?');
+    //         }
+    //     } else {
+    //         return function (name) {
+    //             console.log('Hello ' + name + ', what do you do?');
+    //         }
+    //     }
+    // }
+
+    // // var will equal the function that is returned from the function in the right
+    // var teacherQuestion = interviewQuestion('teacher');
+    // var designerQuestion = interviewQuestion('designer');
+
+    // // The Parameter here will be passed to the function that was returned above
+    // teacherQuestion('John');
+    // designerQuestion('John');
+    // designerQuestion('jane');
+    // designerQuestion('Mark');
+    // designerQuestion('Mike');
+
+    // interviewQuestion('teacher')('Mark');
+
+}
+
+// *****************************************************************
+//    Lecture: IIFE (Imediatley Invoked Function Expression)
+// *****************************************************************
+
+if (ex1) {
+
+    // TODO : What is this for?  
+
+    // function game() {
+    //     var score = Math.random() * 10;
+    //     console.log(score >= 5);
+    // }
+    // game();
+
+
+    // (function () {
+    //     var score = Math.random() * 10;
+    //     console.log(score >= 5);
+    // })();
+
+    // (function (goodLuck) {
+    //     var score = Math.random() * 10;
+    //     console.log(score >= 5 - goodLuck);
+    // })(5);
+
+}
+
+
+// *****************************************************************
+//    Lecture: Closures
+// *****************************************************************
+if (ex1) {
+
+    function retirement(retirementAge) {
+        var a = ' years left until retirement.';
+        return function (yearOfBirth) {
+            var age = 2016 - yearOfBirth;
+            console.log((retirementAge - age) + a);
+        }
+    }
+
+    // TODO : Ask ABout this explanation , Scope chain always stays intact
+    // Although function object is the returned function from 'retirement'
+    // The returned function will still have access to the variables and parameters of its outer function 
+    // Ex: / Still having access to var age = 2016 - yearOfBirth;
+    var retirementUS = retirement(66);
+    var retirementGermany = retirement(65);
+    var retirementIceland = retirement(67);
+
+    retirementGermany(1990);
+    retirementUS(1990);
+    retirementIceland(1990);
+
+    // Can pass arguments in order
+    //retirement(66)(1990);
+
+
+    console.log(`------------------------------------------------`);
+    console.log(`Interview Questions Section `);
+
+    const interviewQuestion = (job) => {
+
+        // Function That will be returned for use, but still have access to job parameter
+        return function (name) {
+
+            if (job === 'designer') {
+                console.log(name + ', can you please explain what UX design is?');
+            }
+            else if (job === 'teacher') {
+
+                console.log('What subject do you teach, ' + name + '?');
+            }
+            else {
+
+                console.log('Hello ' + name + ', what do you do?');
+
+            }
+
         }
 
-        return arrRes;
+        
+
     }
 
-    function calculateAge(argument) {
-        return 2019 - argument;
-    }
+    interviewQuestion('nk')('Alex');
 
-    function isAdult(argument) {
-        return argument >= 18;
-    }
-
-    function maxHeartRate(argument) {
-        if (argument >= 18 && argument <= 81) {
-            return Math.round(206.9 - (0.67 * argument));
-        } else {
-            return -1;
-        }
-    }
-
-    // Call function with same array but different functions
-    var ages = arrayCalc(years, calculateAge);
-    var fullAges = arrayCalc(ages, isAdult);
-    var rates = arrayCalc(ages, maxHeartRate);
-
-    console.log(`Ages: ${ages}`);
-    console.log(`Rates: ${rates}`);
+    // function interviewQuestion(job) {
+    //     if (job === 'designer') {
+    //         return function (name) {
+    //             console.log(name + ', can you please explain what UX design is?');
+    //         }
+    //     } else if (job === 'teacher') {
+    //         return function (name) {
+    //             console.log('What subject do you teach, ' + name + '?');
+    //         }
+    //     } else {
+    //         return function (name) {
+    //             console.log('Hello ' + name + ', what do you do?');
+    //         }
+    //     }
+    // 
 
 }
